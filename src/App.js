1a3dc0e36005185ebfb561bfdb5c9b90482927df
Pandobra <Areeb2009@gmail.com>
2022-10-18 17:41:53 -0700

    pic fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Information } from "./Information";
 import Items from "./Items";
 import Completed from "./Completed";
